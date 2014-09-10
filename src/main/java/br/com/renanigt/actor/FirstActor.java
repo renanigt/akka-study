1d17e0fa0f8326f447d48445bce77e1c6fdb6deb
Renan Montenegro <renanigt@gmail.com>
2014-09-10 00:26:22 -0300

    Adding first actor.

---
+++ src/main/java/br/com/renanigt/actor/FirstActor.java
@@ -0,0 +1,12 @@
+package br.com.renanigt.actor;
+
+import akka.actor.UntypedActor;
+
+public class FirstActor extends UntypedActor {
+
+	@Override
+	public void onReceive(Object message) throws Exception {
+		
+	}
+
+}
